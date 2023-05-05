f09e3747d0302714ad771b2cd0d9032b790f6227
rocean <rocean74@gmail.com>
2023-05-05 05:05:31 +0300

    Count in/out

--- src/main/scala/Graphs.scala
+++ src/main/scala/Graphs.scala
@@ -1,8 +1,38 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, explode, lower, regexp_replace, split}
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Graphs {
     def main(args: Array[String]): Unit = {
+        val sparkConf = new SparkConf().setMaster("local").setAppName("Graphs")
+        val sc = new SparkContext(sparkConf) // create spark context
+
+        val inputFile = "input/web-Stanford.txt"
+//        val inputFile = "input/text.txt"
+
+        val graph = sc.textFile(inputFile)
+            .filter(!_.startsWith("#"))
+
+        val edges = graph.map(line => {
+            val fields = line.split("\t")
+            (fields(0).toInt, fields(1).toInt)
+        })
+
+        println("Κόμβοι με τις περισσότερες εισερχόμενες ακμές")
+
+        // Μέτρηση εισερχόμενων ακμών για κάθε κόμβο και εκτύπωση των 10 πρώτων
+        val inDegrees = edges.map(edge => (edge._2, 1))
+            .reduceByKey(_ + _)
+            .sortBy(_._2, ascending = false)
+
+        inDegrees.take(10).foreach(println)
+
+        println("Κόμβοι με τις περισσότερες εξερχόμενες ακμές")
+
+        // Μέτρηση εξερχόμενων ακμών για κάθε κόμβο και εκτύπωση των 10 πρώτων
+        val outDegrees = edges.map(edge => (edge._1, 1))
+            .reduceByKey(_ + _)
+            .sortBy(_._2, ascending = false)
+
+        outDegrees.take(10).foreach(println)
 
     }
 }
