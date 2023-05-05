efb36c0844f9795d03647eae7f77fe30ce1f0cc3
rocean <rocean74@gmail.com>
2023-05-05 04:20:47 +0300

    Initial commit

---
+++ src/main/scala/Graphs.scala
@@ -0,0 +1,8 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, explode, lower, regexp_replace, split}
+
+object Graphs {
+    def main(args: Array[String]): Unit = {
+
+    }
+}
